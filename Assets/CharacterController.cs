4982eac3d781020205f501899189e5a010a269c8
Sam Pritchard <sampritchard13@gmail.com>
2020-05-25 16:25:20 -0600

    basic horizontal movement and jump added within character controller

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    public float moveSpeed = 5f;
+    public float jumpHeight = 5f;
+    // void Start()
+    // {
+        
+    // }
+
+    void Update() {
+      Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+      transform.position += movement * Time.deltaTime * moveSpeed;
+
+      if ( Input.GetKeyDown("space")) {
+        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpHeight), ForceMode2D.Impulse);
+      }
+    }
+}
