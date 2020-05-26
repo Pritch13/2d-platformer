6d79895c5592eafc491a0ed7823e650a53549e8a
Sam Pritchard <sampritchard13@gmail.com>
2020-05-25 18:34:07 -0600

    added scoreManager and coin script to increment counter

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -5,18 +5,23 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour {
 
     public float moveSpeed = 5f;
-    public float jumpHeight = 5f;
-    // void Start()
-    // {
-        
-    // }
+    public float jumpHeight = 15f;
 
     void Update() {
+      Jump();
       Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
       transform.position += movement * Time.deltaTime * moveSpeed;
-
-      if ( Input.GetKeyDown("space")) {
-        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpHeight), ForceMode2D.Impulse);
-      }
     }
+
+  void Jump() {
+    if(Input.GetKeyDown("space")) {
+      gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpHeight), ForceMode2D.Impulse);
+    }
+  }
+
+  void OnTriggerEnter2D(Collider2D other) {
+    if(other.gameObject.CompareTag("Coins")) {
+      Destroy(other.gameObject);
+    }
+  }
 }
