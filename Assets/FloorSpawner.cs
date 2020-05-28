30a0128dde4ad0c1e4e87ac5a682c80fa3dfe650
Sam Pritchard <sampritchard13@gmail.com>
2020-05-27 19:21:13 -0600

    random spawning grass floor sprites

---
+++ Assets/FloorSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorSpawner : MonoBehaviour
+{
+    
+    
+    public GameObject RandomFloor;
+    public float respawnTime;
+    private Vector2 screenBounds;
+    
+    void Start() {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        StartCoroutine(floorWave());
+    }
+
+    void SpawnFloor() {
+        GameObject newFloor = Instantiate(RandomFloor);
+        newFloor.transform.position = new Vector2(screenBounds.x + 1, 0);
+    }
+    
+    IEnumerator floorWave() {
+        while(true){
+            yield return new WaitForSeconds(Random.Range(0.25f, 1.5f));
+            SpawnFloor();
+        }
+    }
+}
