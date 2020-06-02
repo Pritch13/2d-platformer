13f05481d70cdc4a563636514e71891638cf0f00
Sam Pritchard <sampritchard13@gmail.com>
2020-06-01 21:34:13 -0600

    phone controls added and working with joystick n jump

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -7,6 +7,7 @@ public class CharacterController : MonoBehaviour {
     public float moveSpeed = 5f;
     public float jumpHeight = 15f;
     public bool isGrounded = false;
+    public Joystick joystick;
     public AudioSource coinCollect;
     private Animator anim;
     public Rigidbody2D rb;
@@ -19,12 +20,27 @@ public class CharacterController : MonoBehaviour {
     void Update() {
       TrackMovementForAnimation();
       Jump();
-      Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-      transform.position += movement * Time.deltaTime * moveSpeed;
+      Move();
+    }
+
+    void Move() {
+
+    if(joystick.Horizontal >= .2f || joystick.Horizontal <= -.2f) {
+        Vector3 stickMovement = new Vector3(joystick.Horizontal, 0f, 0f);
+        transform.position += stickMovement * Time.deltaTime * moveSpeed;
+    }
+
+      Vector3 keyMovement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+      transform.position += keyMovement * Time.deltaTime * moveSpeed;
     }
 
     void Jump() {
       if(Input.GetKeyDown("space") && isGrounded) {
+        anim.SetBool("jumped", true);
+        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpHeight), ForceMode2D.Impulse);
+      }
+
+      if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && isGrounded) {
         anim.SetBool("jumped", true);
         gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpHeight), ForceMode2D.Impulse);
       }
@@ -38,7 +54,7 @@ public class CharacterController : MonoBehaviour {
   }
 
   void TrackMovementForAnimation() {
-    if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
+    if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A) || joystick.Horizontal <= -.2f || joystick.Horizontal >= .2f) {
       anim.SetBool("moving", true);
     } else {
       anim.SetBool("moving", false);
@@ -46,10 +62,10 @@ public class CharacterController : MonoBehaviour {
 
     Vector3 characterScale = transform.localScale;
 
-    if(Input.GetAxis("Horizontal") < 0) {
+    if(Input.GetAxis("Horizontal") < 0 || joystick.Horizontal <= -.2f) {
       characterScale.x = -0.7f;
     }
-      if(Input.GetAxis("Horizontal") > 0) {
+      if(Input.GetAxis("Horizontal") > 0 || joystick.Horizontal >= .2f) {
       characterScale.x = 0.7f;
     }
 
